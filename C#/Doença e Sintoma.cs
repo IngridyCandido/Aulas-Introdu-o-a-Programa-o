7bb0dcf2926976daa9280ff853779d693ff685e8
Ingridy Candido <120406194+IngridyCandido@users.noreply.github.com>
2025-05-20 16:56:56 -0300

    Add files via upload

---
+++ C#/Doença e Sintoma.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sint1, sint2;
+            Console.WriteLine("Oi! Soube que está doente, qual o seu sintoma principal?");
+            sint1 = Console.ReadLine();
+            Console.WriteLine("Sinto muito, qual seu segundo sintoma principal?");
+            sint2 = Console.ReadLine();
+
+        }
+    }
+}
