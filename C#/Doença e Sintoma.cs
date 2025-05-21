506a026149450c3876e1c50fe255437f8ff46db8
Ingridy Candido <120406194+IngridyCandido@users.noreply.github.com>
2025-05-21 15:15:51 -0300

    Update Doença e Sintoma.cs

--- C#/Doença e Sintoma.cs
+++ C#/Doença e Sintoma.cs
@@ -1,15 +1,38 @@
-﻿namespace ConsoleApp1
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string sint1, sint2;
+            string sint1, sint2, doença;
             Console.WriteLine("Oi! Soube que está doente, qual o seu sintoma principal?");
             sint1 = Console.ReadLine();
             Console.WriteLine("Sinto muito, qual seu segundo sintoma principal?");
             sint2 = Console.ReadLine();
-
-        }
+            switch (doença)
+            {
+                case 'febre + tosse':
+                    Console.WriteLine("Sua doença provavelmente é: Gripe, resfriado, covid-19 ou dengue.");
+                    break;
+                case '-':
+                    Console.WriteLine("A diferença dos seus números é: " + (num1 - num2) + ".");
+                    break;
+                case '*':
+                    Console.WriteLine("A multiplicação dos seus números é: " + (num1 * num2) + ".");
+                    break;
+                case '/':
+                    if (num2 == 0)
+                    {
+                        Console.WriteLine("Não é posivel dividir por 0.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("A divisão dos seus números é: " + (num1 / num2) + ".");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("A operação escolhida não pode ser executada.");
+                    break;
+            }
     }
 }
