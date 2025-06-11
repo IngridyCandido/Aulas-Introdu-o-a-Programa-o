289a5f49524367510ab24e9d3e5368604e248c18
Ingridy Candido <120406194+IngridyCandido@users.noreply.github.com>
2025-06-11 17:45:16 -0300

    Add files via upload

---
+++ C#/Vetores (Arrays)/Vetores (Arrays)/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Vetores__Arrays_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//inicio
+            int[] x = new int[9];
+            //Lendo valores e preenchendo o vetor.
+            for (int i = 0; i < 9; i++)
+            {
+                Console.WriteLine("Digite um número para a posição " + i + ":");
+                x[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            //Descobrindo valores pares já preenchidos no vetor e quais suas posições.
+            for (int i = 0;i < 9; i++)
+            {
+                if (x[i] % 2 == 0) 
+                { 
+                    Console.WriteLine("O elemento na posição " + i + " é par e é igual a " + x[i]); 
+                }
+            }
+            Console.ReadKey(); //pausa
+        }//fim
+    }
+}
