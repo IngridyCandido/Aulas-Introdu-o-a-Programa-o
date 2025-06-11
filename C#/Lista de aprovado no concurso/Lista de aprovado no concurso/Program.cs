254bd12eb8ae43293b0dd6048c2fa96a66facbce
Ingridy Candido <120406194+IngridyCandido@users.noreply.github.com>
2025-06-11 17:44:34 -0300

    Add files via upload

---
+++ C#/Lista de aprovado no concurso/Lista de aprovado no concurso/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Lista_de_aprovado_no_concurso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] candidato = new string[6];
+            double[] nota = new double[6];
+            for (int i = 0; i < candidato.Length; i++)
+            {
+                Console.WriteLine("Qual o nome do candidado? ");
+                candidato[i] = Console.ReadLine();
+                Console.WriteLine("Qual a nota do candidado? ");
+                nota[i] = Convert.ToDouble(Console.ReadLine());
+            }
+            Console.WriteLine("Qual a posição de classificação você deseja ver? ");
+            int posicao = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("O candidado na "+ posicao + "ª é " + candidato[posicao-1] + " com nota igual a " + nota[posicao-1]);
+            
+        }
+    }
+}
