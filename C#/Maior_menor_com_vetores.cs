ddeac4c348b164aba1dea65c6789b024c6a97b82
Ingridy Candido <120406194+IngridyCandido@users.noreply.github.com>
2025-06-16 16:09:25 -0300

    Add files via upload

---
+++ C#/Maior_menor_com_vetores.cs
@@ -0,0 +1,21 @@
+using System.Diagnostics;
+using System.Net;
+using System.Reflection;
+
+namespace Maior_menor_com_vetores
+{
+    [DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            real [] x = new real x [15];
+            for (int i = 0; i <x.Length; i++) 
+            {
+                Console.WhiteLine("Digite um número real: ");
+                x[i] = Convert.ToReal(Console.ReadLine());
+                if x>
+            }
+        }
+    }
+}
