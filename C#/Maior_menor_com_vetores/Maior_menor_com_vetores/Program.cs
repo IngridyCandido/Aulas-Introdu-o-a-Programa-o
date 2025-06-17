aa14c860d3379661247684e8adfcb84d6d523463
Ingridy Candido <120406194+IngridyCandido@users.noreply.github.com>
2025-06-17 00:00:36 -0300

    Add files via upload

---
+++ C#/Maior_menor_com_vetores/Maior_menor_com_vetores/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Maior_menor_com_vetores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] x = new double [15];
+            double maiornumero = -99999999999;
+            double menornumero = 999999999999;
+            int imaior = 0;
+            int imenor = 0;
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                Console.WriteLine("Digite um número real: ");
+                x[i] = Convert.ToDouble(Console.ReadLine());
+                if (x[i] > maiornumero)
+                {
+                    maiornumero = x[i];
+                    imaior = i;
+                }
+                if (x[i] < menornumero)
+                {
+                    menornumero = x[i];
+                    imenor = i;
+                }
+            }
+            Console.WriteLine("O maior número do vetor é " + maiornumero + " e sua posição é " + imaior + ".");
+            Console.WriteLine("O menor número do vetor é " + menornumero + " e sua posição é " + imenor + ".");
+        }
+    }
+}
