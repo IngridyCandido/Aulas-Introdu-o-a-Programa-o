add158489c95326770200e3bc804b5e3a5b6df5e
Ingridy Candido <120406194+IngridyCandido@users.noreply.github.com>
2025-06-16 22:12:47 -0300

    Add files via upload

--- C#/Maior_menor_com_vetores.cs
+++ C#/Maior_menor_com_vetores.cs
@@ -9,13 +9,32 @@ namespace Maior_menor_com_vetores
     {
         static void Main(string[] args)
         {
-            real [] x = new real x [15];
+            double [] x = new double x [15];
+            double maiornumero = -99999999999; 
+            double menornumero = 999999999999;
             for (int i = 0; i <x.Length; i++) 
             {
                 Console.WhiteLine("Digite um número real: ");
-                x[i] = Convert.ToReal(Console.ReadLine());
-                if x>
+                x[i] = Convert.ToDouble(Console.ReadLine());
+                if (x[i] > maiornumero) 
+                {
+                    maiornumero = Convert.ToDouble(Console.ReadLine());
+                }
+                if (x[i] < menornumero)
+                {
+                    menornumero = Convert.ToDouble(Console.ReadLine());
+                }     
             }
+            for (x[i]=maiornumero)
+            {
+            Console.WhiteLine("O maior número do vetor é " + maiornumero + "e sua posição é " + i)
+            }
+            proximo
+            for (x[i]=menornumero)
+            {
+            Console.WhiteLine("O menor número do vetor é " + menornumero + "e sua posição é " + i)
+            }
+            proximo
         }
     }
 }
