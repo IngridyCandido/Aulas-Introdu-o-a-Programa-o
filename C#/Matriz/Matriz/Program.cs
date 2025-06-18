38b115a00779ee3ab9af5fc950637a5d1a911b7b
Ingridy Candido <120406194+IngridyCandido@users.noreply.github.com>
2025-06-18 16:24:31 -0300

    Add files via upload

---
+++ C#/Matriz/Matriz/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Matriz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] x =new int[2,5];
+            //carregando elementos para uma matriz
+            for (int i = 0; i < x.GetLength(0); i++)
+            {
+                for (int j = 0; j < x.GetLength(1); j++)
+                {
+                    Console.WriteLine("Digite o número da linha " + i + "e coluna " + j + ": "];
+                    x[i,j] = Convert.ToInt32(Console.ReadLine();
+                }
+            //imprimindo elementos de uma matriz
+            for (int i = 0; i < i<=1; i++)
+            {
+                for (int j = 0; j < x.GetLength(1); j++)
+                {
+                        Console.WriteLine("x[" + i + 
+                }
+        }
+    }
+}
