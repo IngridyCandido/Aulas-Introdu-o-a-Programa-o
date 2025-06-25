02c8f867815bcd15a6fdf3937d18c561ed1d84fa
Ingridy Candido <120406194+IngridyCandido@users.noreply.github.com>
2025-06-25 17:12:04 -0300

    Add files via upload

---
+++ C#/Matriz 3x2x2/Matriz 3x2x2/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Matriz_3x2x2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,,] x = new int[3, 2, 2];
+            for (int i = 0; i < x.GetLength(0); i++)
+            {
+                for (int j = 0; j < x.GetLength(1); j++)
+                {
+                    for (int k = 0; k < x.GetLength(2); k++)
+                    {
+                        Console.WriteLine("Digite um número para 1 linha " + i + " um para coluna " + j + " e um para diagonal " + k + ":");
+                        x[i, j, k] = Convert.ToInt32(Console.ReadLine()); 
+                    }
+
+                }
+
+            }
+            for (int i = 0; i < x.GetLength(0); i++)
+            {
+                for (int j = 0; j < x.GetLength(1); j++)
+                {
+                    for (int k = 0; k < x.GetLength(2); k++)
+                    {
+                        Console.WriteLine("x[" + i + "," + j + "," + k + "] vale " + x[i,j,k] + ".");
+                    }
+
+                }
+
+            }
+        }
+    }
+}
---
+++ C#/Triangulo superior de matriz/Triangulo superior de matriz/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Triangulo_superior_de_matriz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] x = new int[3,3];
+            char opcao;
+            for (int i = 0; i < x.GetLength(0); i++)
+            {
+                for (int j = 0; j < x.GetLength(1); j++)
+                {
+                        Console.WriteLine("Digite um número para 1 linha " + i + " um para coluna " + j + ":");
+                        x[i,j] = Convert.ToInt32(Console.ReadLine());
+
+                }
+
+            }
+
+            Console.WriteLine("Menu de Opções: \n a – triângulo superior \n b – triângulo inferior \n c – diagonal principal \n Digite uma opção:");
+            opcao = Convert.ToChar(Console.ReadLine());
+
+            for (int i = 0; i < x.GetLength(0); i++)
+            {
+                switch (opcao)
+                {
+                    case 'a':
+                        for (int j = i; j < x.GetLength(1); j++)
+                        {
+                            Console.WriteLine("O triângulo superior da matriz é composto por: " + x[i, j]);
+                        }
+                        break;
+                    case 'b':
+                        for (int j = 0 ; j <= i; j++)
+                        {
+                            Console.WriteLine("O triângulo inferior da matriz é composto por: " + x[i, j]);
+                        }
+                            break; 
+                    case 'c':
+                         for (int j = i ; j == i; j++)
+                         {
+                            Console.WriteLine("A diagonal principal da matriz é composto por: " + x[i, j]);
+                         }
+                         break;
+                }
+
+            }
+        }
+    }
+}
