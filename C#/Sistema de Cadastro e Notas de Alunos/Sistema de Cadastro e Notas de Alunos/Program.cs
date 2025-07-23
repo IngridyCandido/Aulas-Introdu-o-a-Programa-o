0562880d827b89e04f30ca49839b3384be0d55d7
Ingridy Candido <120406194+IngridyCandido@users.noreply.github.com>
2025-07-23 10:56:26 -0300

    Add files via upload

---
+++ C#/Sistema de Cadastro e Notas de Alunos/Sistema de Cadastro e Notas de Alunos/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Runtime.Serialization;
+
+namespace Sistema_de_Cadastro_e_Notas_de_Alunos
+{
+    internal class Program
+    {
+        struct Aluno
+        {
+            public string Nome;
+            public int Matricula;
+            public double MediaFinal;
+        }
+
+        static void Main(string[] args)
+        {
+            Aluno[] alunos = new Aluno[5];
+            for (int i = 0; i < alunos.Length; i++)
+            {
+                Console.WriteLine($"Digite o nome do aluno {i + 1}:");
+                alunos[i].Nome = Console.ReadLine();
+
+                Console.WriteLine($"Digite a matrícula do aluno {i + 1}:");
+                alunos[i].Matricula = int.Parse(Console.ReadLine());
+            }
+
+            double[,] notas = new double[5,3];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Aluno: {alunos[i].Nome} (Matrícula: {alunos[i].Matricula})");
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine($"Digite a nota {j+1} do aluno {i+1}:");
+                    notas[i, j] = double.Parse(Console.ReadLine());
+
+                }
+            }
+            for (int j = 0; j < alunos.Length; j++)
+            {
+                double soma = 0;
+                for (int k = 0; k < 3; k++)
+                {
+                    soma += notas[j, k];
+                }
+                alunos[j].MediaFinal = soma / 3;
+                Console.WriteLine($"A média final do aluno {alunos[j].Nome} é: {alunos[j].MediaFinal}");
+            }
+        }
+    }
+}
