5832cd24df8452f4d51e4bde847e1d3706e76e0c
Ingridy Candido <120406194+IngridyCandido@users.noreply.github.com>
2025-07-30 16:13:08 -0300

    Add files via upload

---
+++ C#/Sub-Rotinas/Sub-Rotinas/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Sub_Rotinas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double sal, aum, novoSal;
+            Imprimir("Digite o salário atual: ");
+            sal =Convert.ToDouble(Console.ReadLine());
+            aum = Calculo(sal);
+            novoSal = sal + aum;
+            Imprimir("O novo salário é: " + novoSal);
+        }
+        static double Calculo(double sal)
+        {
+            double perc, valor;
+            Imprimir("Digite o percentual de aumento: ");
+            perc = Convert.ToDouble(Console.ReadLine());
+            valor = sal * perc / 100;
+            return valor;
+        }
+        static void Imprimir (string texto)
+        {
+            Console.WriteLine(texto);
+        }
+    }
+}
